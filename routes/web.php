ed40e094d7ca20023dc621443b90d6fb02b7b90c
Kristi <kristimihali3@gmail.com>
2020-03-03 13:11:20 +0100

    Authentifications and routers

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// Route::get('/', 'HomePageController@index');
+Route::get('/articles', 'ArticlesController@index');
+Route::get('/contact', 'ContactController@index');
+Route::get('/articles/{post_name}', 'ArticlesController@show');
+
+Route::post('contact', 'ContactController@save')->name('contact.store');
+
+Auth::routes();
+
+// Route::get('/profile', 'HomeController@profile');
+Route::get('/', 'HomeController@index');
+Route::get('/create', 'ArticlesController@create');
+Route::get('/home', 'HomeController@index');
+
