8b5922b5b3036fde0e1f8336d97e9d267b7544ac
Zhao ZHANG <zo.zhang@gmail.com>
2020-04-08 01:21:42 +0200

    Adding Laravel Mix && Sass

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -2,23 +2,20 @@
 
 @section('content')
 
-<div class = 'container'>
-    <div class = 'row'>
-        <div class = 'col-md-12'>
-            
-            <div class = "pull-left">
-                <marquee behavior ="" direction="">CRUD ARTICLES USER PAGE</marquee>
-            </div>
-            <br>
-            <div class = "pull-right">
-                <a href = "/create" class = "btn btn-lg btn-success">Add New Article</a>
-            </div>
-
+    <div clas ='col-md-12'>
+        <div class = "pull-left">
+            <marquee behavior="" direction="">CRUD ARTICLES USER PAGE</marquee>
+        </div>
+        <br>
+        <div class="pull-right">
+            <a href="/create" class = "btn btn-lg btn-success">Add New Article</a>
         </div>
     </div>
+
     @if($errors->any())
         <h6>{{$errors->first()}}</h6>
     @endif
+
     <table class="table table-bordered table-dark">
         <tr>   
             <th>Name</th>
@@ -41,15 +38,13 @@
                 <button type="submit" class="btn btn-danger">Delete</button>
                 <a href = "/articles/<?=$post->post_name?>" class="btn btn-danger"> Show</a>
             </form>
-            <!-- <form method="POST" action="{{ route('admin.edit',$post->id) }}">
+            <form method="POST" action="{{ route('admin.edit',$post->id) }}">
                 @method('PUT')
                 @csrf
-                <a href = "/edit/" class="btn btn-danger"> Edit</a>
-            </form> -->
+                <a href = "/edit/<?=$post->id?>" class="btn btn-danger"> Edit</a>
+            </form>
             </td>
         </tr>
         @endforeach
     </table>
-</div>
-
 @endsection
