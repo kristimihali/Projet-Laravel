b8be78baea1e35c3f65f3d7ca77e08a75c0a4e5f
Zhao ZHANG <zo.zhang@gmail.com>
2020-04-15 20:05:01 +0200

    Update Facker ImageUrl Libraray

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -13,6 +13,27 @@ use Faker\Generator as Faker;
 | model instances for testing / seeding your application's database.
 |
 */
+function randomnum($number)
+{
+    $max = pow(10, $number) - 1;
+    $randn = mt_rand(0, $max);
+
+    if (in_array($randn, [138, 333, 470, 644])) {
+        return randomnum($number);
+    }
+    return $randn;
+}
+
+function imageUrl($width = 640, $height = 480, $category = null, $randomize = true, $word = null, $gray = false)
+{
+    if ($randomize) {
+        $randomize_number =  randomnum(3);
+    }
+
+    $baseUrl = "https://i.picsum.photos/id/" . $randomize_number . "/";
+    $url = "{$width}/{$height}.jpg";
+    return $baseUrl . $url;
+}
 
 $factory->define(App\Post::class, function (Faker $faker) {
     $users = App\User::pluck('id')->toArray();
@@ -23,7 +44,8 @@ $factory->define(App\Post::class, function (Faker $faker) {
         'post_status' => $faker->randomElement(['PUBLIED','DRAFT']),
         'post_name' => $faker->firstName(),
         'post_type' => 'article',
-        'cover_image'  => $faker->imageUrl(),
+        'cover_image'  => imageUrl(),
         'post_category' => $faker->randomElement(['vie', 'sociale', 'actualiste']),
     ];
 });
+
